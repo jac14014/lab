611176dfef602fd20eedeb793115445077f7ab4e
Jacqueline A. Ennis <jac14014@Jacquelines-MacBook-Air.local>
2026-07-10 17:37:54 -0500

    Add institute governance pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,21 @@ import { defineConfig } from "vite"
 export default defineConfig({
   base: './',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        about: path.resolve(__dirname, "about.html"),
+        governance: path.resolve(__dirname, "governance.html"),
+        donate: path.resolve(__dirname, "donate.html"),
+        sitemap: path.resolve(__dirname, "sitemap.html"),
+        contact: path.resolve(__dirname, "contact.html"),
+        articlesOfIncorporation: path.resolve(__dirname, "articles-of-incorporation.html"),
+        bylaws: path.resolve(__dirname, "bylaws.html"),
+        orgChart: path.resolve(__dirname, "org-chart.html"),
+      },
+    },
+  },
   server: {
     port: 3000,
   },
